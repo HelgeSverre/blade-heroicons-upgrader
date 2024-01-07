9307042db7aafc325556b8057b2e4e07c2fc0417
Helge Sverre <helge.sverre@gmail.com>
2024-01-07 14:13:15 +0100

    use laravel promtps for prettier output

--- src/Commands/UpgradeIcons.php
+++ src/Commands/UpgradeIcons.php
@@ -2,50 +2,72 @@
 
 namespace HelgeSverre\BladeHeroiconsUpgrader\Commands;
 
+use HelgeSverre\BladeHeroiconsUpgrader\Data\Replacement;
 use HelgeSverre\BladeHeroiconsUpgrader\IconReplacer;
 use Illuminate\Console\Command;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\File;
+use Laravel\Prompts\Progress;
 
-use function config;
+use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\error;
+use function Laravel\Prompts\info;
+use function Laravel\Prompts\note;
+use function Laravel\Prompts\progress;
+use function Laravel\Prompts\table;
 
 class UpgradeIcons extends Command
 {
-    protected $signature = 'blade-heroicons-upgrader:upgrade {paths?*} {--dry : Perform a dry run without actually replacing any icons}';
+    protected $signature = 'blade-heroicons-upgrader:upgrade {paths?*} {--dry : Perform a dry run without actually replacing any icons} ';
 
-    protected $description = 'Replace old icon names with new ones';
-
-    public static array $variants = ['o', 's', 'm'];
+    protected $description = 'Replace references to Heroicons v1 icon names with Heroicon v2 icon names';
 
     public function handle()
     {
+        $start = hrtime(true);
         $paths = Arr::wrap($this->argument('paths'));
 
         if (empty($paths)) {
             $paths = ['./resources/views'];
-            $this->comment("No path(s) provided, using default: {$paths[0]}");
+            $confirmed = confirm(
+                label: 'No path(s) provided, use defaults?',
+                hint: "Default path: {$paths[0]}"
+            );
+
+            if (! $confirmed) {
+                error('Aborting');
+
+                return;
+            }
+
+            info("Using default path: {$paths[0]}");
+
         }
 
-        $totalReplaced = 0;
-        $totalFilesWithReplacements = 0;
+        /**
+         * @var Replacement[] $replacements
+         */
+        $replacements = [];
 
         $iconReplacer = new IconReplacer(
             config('blade-heroicons-upgrader.replacements')
         );
 
-        foreach ($paths as $path) {
+        $allFiles = collect($paths)->flatMap(function ($path) {
             $realPath = realpath($path);
-
-            if (! File::exists($realPath)) {
-                $this->error("The path {$path} does not exist.");
-
-                return;
+            if (File::missing($realPath)) {
+                return null;
             }
 
-            $files = File::isFile($realPath) ? [$realPath] : File::allFiles($realPath);
+            return File::isFile($realPath) ? [$realPath] : File::allFiles($realPath);
+        })->filter()->values();
 
-            foreach ($files as $file) {
-                $this->info("{$file}");
+        progress(
+            label: 'Processing files',
+            steps: $allFiles,
+            callback: function (string $file, Progress $progress) use ($iconReplacer, &$replacements) {
+
+                $progress->hint("Processing: $file");
 
                 $contents = File::get($file);
 
@@ -53,19 +75,36 @@ class UpgradeIcons extends Command
 
                 if (! $this->option('dry')) {
                     File::put($file, $info->new);
+
+                    foreach ($info->adjustedReplacements as $replacement) {
+                        $replacements[] = $replacement;
+                    }
                 }
 
-                $totalReplaced += $info->count();
-
-                if ($info->isNotEmpty()) {
-                    $totalFilesWithReplacements++;
-                    $this->comment("> Replaced {$info->count()} icons.\n");
+                foreach ($info->replacements as $replacement) {
+                    $replacements[] = $replacement;
                 }
+            },
+        );
 
-            }
+        table(
+            ['Old icon', 'New Icon', 'Location'],
+            array_map(function ($replacement) {
+                $relativePath = str_replace(getcwd().'/', '', $replacement->filePath);
 
-        }
+                return [
+                    $replacement->oldIcon,
+                    $replacement->newIcon,
+                    "{$relativePath}:{$replacement->line}:{$replacement->column}",
+                ];
+            }, $replacements)
+        );
 
-        $this->comment("\n\nDONE: Replaced {$totalReplaced} icons across {$totalFilesWithReplacements} files.");
+        $replacementCount = count($replacements);
+
+        $timeInSec = round((hrtime(true) - $start) / 1000000000, 2);
+
+        info("Replaced {$replacementCount} icons across {$allFiles->count()} files.");
+        note("Took {$timeInSec} seconds");
     }
 }
